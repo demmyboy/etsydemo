b9dd26584dbf2cbea573b56ca1bd1ab371071094
Demmy Abdul <demmyboy@yahoo.com>
2014-07-09 22:04:29 +0200

    first change

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Etsydemo::Application.config.secret_key_base = '2457a4e327d97bab08e09bc820ad700e54ef8ab73c1ace2b522c6b53ec51fd95fdf9615c3f0affa2db71eaa451fdf6151d61a65669100970e4eb133ce63e5cf5'
